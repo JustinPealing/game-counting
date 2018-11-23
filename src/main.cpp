7a6a7bd0f8e35383d54b3ffaeed6f10f70f6be28
Justin Pealing <justin@pealing.me.uk>
2018-11-23 21:23:48 +0000

    Fix window title

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ const float window_height = 960;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Jumping Game");
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Counting Game");
     sf::Clock clock;
     window.setFramerateLimit(30);
 
